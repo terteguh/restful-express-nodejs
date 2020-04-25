f1823a849bc09c18524dc45e5a0bf63825799d04
teguh fihaiman <teguh.fihaiman@mii.co.id>
2020-04-25 21:31:39 +0700

    not yet finish database call doesnt finish

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://teguh:JIDYcrb7VOUdegxZ@cppi-shard-00-00-noe9p.mongodb.net:27017,cppi-shard-00-01-noe9p.mongodb.net:27017,cppi-shard-00-02-noe9p.mongodb.net:27017/test?ssl=true&replicaSet=Cppi-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -10,7 +11,17 @@ const port = process.env.PORT || 3000;
 
 const Book = require('./models/bookModel');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 bookRouter.route('/books')
+  .post((req, res) =>{
+    const book = new Book(req.body);
+
+    // eslint-disable-next-line no-console
+    console.log(book);
+    return res.json(book);
+  })
   .get((req, res) => {
     // const { query } = req;
     const query = {};
