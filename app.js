a9af4c5407b262e8b044776bc496cd6d23e697ac
teguh fihaiman <teguh.fihaiman@mii.co.id>
2020-04-25 16:50:30 +0700

    create model Book

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = mongoose.connect('mongodb://teguh:JIDYcrb7VOUdegxZ@cppi-shard-00-00-noe9p.mongodb.net:27017,cppi-shard-00-01-noe9p.mongodb.net:27017,cppi-shard-00-02-noe9p.mongodb.net:27017/test?ssl=true&replicaSet=Cppi-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const bookRouter = express.Router();
 
 const port = process.env.PORT || 3000;
 
+const book = require('./models/bookModel');
+
 bookRouter.route('/books')
   .get((req, res) => {
-    const response = { hello: 'This is API Book'};
+    const response = { hello: 'This is API Book' };
     res.json(response);
   });
 app.use('/api', bookRouter);
