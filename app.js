6ed793c9cead1034fc2ca0bcac368e9089d6915c
teguh fihaiman <teguh.fihaiman@mii.co.id>
2020-04-25 16:17:12 +0700

    json api with const response

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express');
 
 const app = express();
 
+const bookRouter = express.Router();
+
 const port = process.env.PORT || 3000;
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    const response = { hello: 'This is API Book'};
+    res.json(response);
+  });
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send(`Wellcome to d API ${port}`);
