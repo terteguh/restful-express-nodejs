e9bcaa179b7990a49c2dee42f4fd9da3b4644d5a
teguh fihaiman <teguh.fihaiman@mii.co.id>
2020-04-25 16:52:46 +0700

    implement comma dangle

--- models/bookModel.js
+++ models/bookModel.js
@@ -7,6 +7,6 @@ const bookModel = new Schema(
     title: { type: String },
     author: { type: String },
     genre: { type: String },
-    read: { type: Boolean, default: false },
-  },
+    read: { type: Boolean, default: false }
+  }
 );
