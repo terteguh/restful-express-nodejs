f8947b3effe05cda116b0990075a43c75ae1992d
teguh fihaiman <teguh.fihaiman@mii.co.id>
2020-04-25 17:18:52 +0700

    fixing running get book

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ const bookRouter = express.Router();
 
 const port = process.env.PORT || 3000;
 
-const book = require('./models/bookModel');
+const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
   .get((req, res) => {
-    const response = { hello: 'This is API Book' };
-    res.json(response);
+    Book.find((err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
   });
 app.use('/api', bookRouter);
 
