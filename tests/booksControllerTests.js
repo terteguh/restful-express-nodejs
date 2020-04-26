a78ea9e65a6eeff0d02d8e8daac0f6c8605aa7e8
teguh fihaiman <teguh.fihaiman@mii.co.id>
2020-04-26 15:51:16 +0700

    Testing w Mocha 1

--- tests/booksControllerTests.js
+++ tests/booksControllerTests.js
@@ -5,7 +5,24 @@ const bookController = require('../controllers/booksController');
 describe('Book Controller Test:', () => {
   describe('Post', () => {
     it('should not allow an empty title on post', () => {
+      const Book = function (book) {
+        this.save = () => {};
+      };
+      const req = {
+        body: {
+          author: 'Jon'
+        }
+      };
+      const res = {
+        status: sinon.spy(),
+        send: sinon.spy(),
+        json: sinon.spy()
+      };
+      const controller = bookController(Book);
+      controller.post(req, res);
 
+      res.status.calledWith(400).should.equal(true, `Bad Status ${res.status.args[0][0]}`);
+      res.send.calledWith('Title is required').should.equal(true);
     });
   });
 });
