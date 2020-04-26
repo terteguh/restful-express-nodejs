12f1d5f156a2afebe6e4cf046f649ca059dbc531
teguh fihaiman <teguh.fihaiman@mii.co.id>
2020-04-26 16:07:54 +0700

    Running test after fixing

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,7 +1,10 @@
 function booksController(Book) {
   function post(req, res) {
     const book = new Book(req.body);
-
+    if (!req.body.title) {
+      res.status(400);
+      return res.send('Title is required');
+    }
     book.save();
     res.status(201);
     return res.json(book);
