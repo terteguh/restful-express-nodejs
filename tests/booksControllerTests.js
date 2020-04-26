5a919bb8d71bf461b9b8a08abfdc9f50452586ae
teguh fihaiman <teguh.fihaiman@mii.co.id>
2020-04-26 15:22:49 +0700

    Create Testing w Mocha 1 install

---
+++ tests/booksControllerTests.js
@@ -0,0 +1,14 @@
+const should = require('should');
+const sinon = require('sinon');
+const bookController = require('../controllers/booksController');
+
+describe('Book Controller Test:', () => {
+  describe('Post', () => {
+    it('should not allow an empty title on post', () => {
+
+    });
+
+  });
+
+
+});
